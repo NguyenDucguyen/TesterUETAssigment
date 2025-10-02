cb0650e3c4afafa1ef7b9cd2bdfa9463cef5d4ea
NguyenDucguyen <nguyenhaibatrung05@gmail.com>
2025-10-02 10:13:09 +0700

    feat: add new file

---
+++ src/main/java/com/showtime/Bomberman.java
@@ -0,0 +1,25 @@
+package com.showtime;
+
+public class Bomberman {
+    public static int TinhDiem(int bricks, int enemies, int time) {
+
+        final int BRICK_POINT = 10;
+        final int ENEMY_POINT = 100;
+
+        int brickScore = bricks * BRICK_POINT;
+        int enemyScore = enemies * ENEMY_POINT;
+
+        if (enemies >= 5) {
+            enemyScore = (int)(enemyScore * 1.5);
+        }
+
+        int totalScore = brickScore + enemyScore;
+
+        if (time < 60) {
+            totalScore = (int)(totalScore * 1.2);
+        } else if (time <= 180) {
+            totalScore = (int)(totalScore * 1.1);
+        }
+        return totalScore;
+    }
+}
